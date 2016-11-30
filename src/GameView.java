c39653f9c52ff1cbcbf77967269b590e71506c4d
Lapper <Lapper@104.39.97.46>
2016-11-30 15:51:20 -0500

    basic game play utility

--- src/GameView.java
+++ src/GameView.java
@@ -12,14 +12,12 @@ import javax.swing.JTextField;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author ohs5008
  */
 public class GameView extends JPanel {
-    
-    
+
     JLabel label;
     JButton next;
     JLabel questionLabel;
@@ -28,47 +26,79 @@ public class GameView extends JPanel {
     JLabel answerThreeLabel;
     JLabel answerFourLabel;
     JLabel correctAnswerLabel;
+    int currentQuestionNumber;
+    int totalQuestionNumber;
 
     JTextField userAnswer;
-    GridLayout grid = new GridLayout(10,3);
-        ArrayList<String> questionList = new ArrayList();
-    ArrayList<String> answerList = new ArrayList();
+    GridLayout grid = new GridLayout(10, 3);
+    ArrayList<String> questionList = new ArrayList();
+    ArrayList<String> answerOneList = new ArrayList();
+    ArrayList<String> answerTwoList = new ArrayList();
+    ArrayList<String> answerThreeList = new ArrayList();
+    ArrayList<String> answerFourList = new ArrayList();
     ArrayList<String> correctAnswerList = new ArrayList();
+    ArrayList<ArrayList> answerList = new ArrayList();
 
+    GameView() {
+        answerList.add(answerOneList);
+        answerList.add(answerTwoList);
+        answerList.add(answerThreeList);
+        answerList.add(answerFourList);
 
-    GameView(){
         questionList.add("What is my name?");
-        answerList.add("Billy");
+        answerList.get(0).add("Willy");
+  //      [[a],[a]]
+    //    [[b],[b]]
+      //  [[c],[c]]
+        //[[d],[d]]
+        //[   [[a],[b],[c],[d]],   [[a],[b],[c],[d]] ,  [[a],[b],[c],[d]] ]
+       
         
-        answerList.add("Willy");
+        answerList.get(1).add("Nilly");
+        answerList.get(2).add("Silly");
+        answerList.get(3).add("Rilly");
+        correctAnswerList.add("Timmy");
+
+        questionList.add("What is your name?");
+        answerList.get(0).add("Baby");
+        answerList.get(1).add("Child");
+        answerList.get(2).add("Haily");
+        answerList.get(3).add("Tommy");
+        correctAnswerList.add("You Have None.");
+
+        questionList.add("What is my name?");
+        answerList.get(0).add("Willy");
+        answerList.get(1).add("Nilly");
+        answerList.get(2).add("Silly");
+        answerList.get(3).add("Rilly");
+        correctAnswerList.add("Timmy");
+
+        generateQuestion();
         
-        answerList.add("Nilly");
         
-        answerList.add("Silly");
-        
-        correctAnswerList.add("Willy");
-    setLayout(grid);
-    label = new JLabel("Working");
-    next = new JButton("Next");
-    questionLabel = new JLabel(questionList.get(0));
-    answerOneLabel = new JLabel(answerList.get(0));
-    answerTwoLabel = new JLabel(answerList.get(1));
-    answerThreeLabel = new JLabel(answerList.get(2));
-    answerFourLabel = new JLabel(answerList.get(3));
-    userAnswer = new JTextField();
-    correctAnswerLabel = new JLabel();
-    
-    add(label);
-    add(questionLabel);
-    add(answerOneLabel);
-    add(answerTwoLabel);
-    add(answerThreeLabel);
-    add(answerFourLabel);
-    add(userAnswer);
-    add(new JLabel());
-    add(next);
-    add(correctAnswerLabel);
-    
+        setLayout(grid);
+        label = new JLabel("Working");
+        next = new JButton("Next");
+        questionLabel = new JLabel();
+        answerOneLabel = new JLabel();
+        answerTwoLabel = new JLabel();
+        answerThreeLabel = new JLabel();
+        answerFourLabel = new JLabel();
+
+        userAnswer = new JTextField();
+        correctAnswerLabel = new JLabel();
+
+        add(label);
+        add(questionLabel);
+        add(answerOneLabel);
+        add(answerTwoLabel);
+        add(answerThreeLabel);
+        add(answerFourLabel);
+        add(userAnswer);
+        add(new JLabel());
+        add(next);
+        add(correctAnswerLabel);
+
     }
 
     public JLabel getLabel() {
@@ -102,8 +132,43 @@ public class GameView extends JPanel {
     public void setNext(JButton next) {
         this.next = next;
     }
-    
+
     public void addTestButtonListener(ActionListener al) {
         this.next.addActionListener(al);
     }
+
+    public void generateQuestion() {
+
+        for (int i = 0; i < questionList.size(); i++) {
+        }
+        currentQuestionNumber = 1;
+        totalQuestionNumber = questionList.size() + 1;
+
+    }
+
+    public void setQuestion(int questionNumber) {
+        int i = questionNumber - 1;
+        questionLabel.setText(questionList.get(i));
+        answerOneLabel.setText((String) answerList.get(0).get(i));
+        answerTwoLabel.setText((String) answerList.get(1).get(i));
+        answerThreeLabel.setText((String) answerList.get(2).get(i));
+        answerFourLabel.setText((String) answerList.get(3).get(i));
+    }
+
+    public int getCurrentQuestionNumber() {
+        return currentQuestionNumber;
+    }
+
+    public void setCurrentQuestionNumber(int currentQuestionNumber) {
+        this.currentQuestionNumber = currentQuestionNumber;
+    }
+
+    public int getTotalQuestionNumber() {
+        return totalQuestionNumber;
+    }
+
+    public void setTotalQuestionNumber(int totalQuestionNumber) {
+        this.totalQuestionNumber = totalQuestionNumber;
+    }
+
 }
