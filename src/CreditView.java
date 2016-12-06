b8c1ed13cec38f3896629f31400c0179491883f6
ohs5008 <ohs5008@202-WD04.up.ist.local>
2016-12-06 18:44:06 -0500

    JCombobox

--- src/CreditView.java
+++ src/CreditView.java
@@ -1,5 +1,6 @@
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -10,5 +11,12 @@ public class CreditView extends JPanel {
     CreditView() {
         text = new JTextArea("Credits:\n\n * Dhaka Kharel \n\n * Oscar Sanjuan \n\n * Andy Lam ");
         add(text);
+        
+        String[] subjects = new String[] {"Math", "Geography", "Science"};
+        JComboBox<String> subjectList = new JComboBox<>(subjects);
+        
+        add(subjectList);
+        String selectSubject = (String)subjectList.getSelectedItem();
+        System.out.println("Subject selected: " + selectSubject);
     }
 }
